375fdf6f5a29ca1a47494878fe3fdad78cbfad09
kayanm689 <kayanm689@gmail.com>
2026-02-14 16:30:31 -0500

    Lesson2

--- Assets/Geo_Quest/Scripts/GeoController.cs
+++ Assets/Geo_Quest/Scripts/GeoController.cs
@@ -4,10 +4,13 @@ using UnityEngine;
 
 public class GeoController : MonoBehaviour
 {
+    string VariableOne = "Hello ";
     // Start is called before the first frame update
     void Start()
     {
-        
+        string VariableTwo = "World";
+
+        Debug.Log(VariableOne + VariableTwo);   
     }
 
     // Update is called once per frame
